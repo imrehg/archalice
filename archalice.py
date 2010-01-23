114a6eb827c0e3dd4557aee8f76fde1bbd111bb9
Gergely Imreh <imrehg@gmail.com>
2010-01-24 00:07:59 +0800

    Update indentation to 4 spaces

--- archalice.py
+++ archalice.py
@@ -7,24 +7,24 @@ import sys
 from threading import Thread
 
 class testit(Thread):
-   def __init__ (self,ip):
-      Thread.__init__(self)
-      self.ip = ip
-      self.status = -1
-      self.responsetime = -1
+    def __init__ (self,ip):
+        Thread.__init__(self)
+        self.ip = ip
+        self.status = -1
+        self.responsetime = -1
 
-   def run(self):
-      pingaling = os.popen("ping -q -c2 "+self.ip,"r")
-      while 1:
-        line = pingaling.readline()
-        if not line: break
-        igot = re.findall(testit.lifeline,line)
-        if igot:
-            self.status = int(igot[0])
+    def run(self):
+        pingaling = os.popen("ping -q -c2 "+self.ip,"r")
+        while 1:
             line = pingaling.readline()
-            restime = re.search(testit.response, line)
-            if  restime:
-                self.responsetime = restime.group(1)
+            if not line: break
+            igot = re.findall(testit.lifeline,line)
+            if igot:
+                self.status = int(igot[0])
+                line = pingaling.readline()
+                restime = re.search(testit.response, line)
+                if  restime:
+                    self.responsetime = restime.group(1)
 
 testit.lifeline = re.compile(r"(\d) received")
 testit.response = re.compile(r'((\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?) ms')
@@ -35,14 +35,13 @@ print time.ctime()
 pinglist = []
 
 for host in range(1,10):
-   ip = "192.168.11."+str(host)
-   current = testit(ip)
-   pinglist.append(current)
-   current.start()
+    ip = "192.168.11."+str(host)
+    current = testit(ip)
+    pinglist.append(current)
+    current.start()
 
 for pingle in pinglist:
-   pingle.join()
-   print "Status from ",pingle.ip,"is",report[pingle.status],"time:",pingle.responsetime
+    pingle.join()
+    print "Status from ",pingle.ip,"is",report[pingle.status],"time:",pingle.responsetime
 
 print time.ctime()
-
